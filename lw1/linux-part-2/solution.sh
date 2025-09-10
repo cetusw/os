f6711eb6b93aae2e70c902aa59f31ff7d196d3a0
cetusw <kugelev.m@gmail.com>
2025-09-10 22:30:17 +0300

    linux-part-2 правки

---
+++ lw1/linux-part-2/solution.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+if test -d out; then
+    rm -rf out
+fi
+
+mkdir out
+
+cd out
+
+touch me.txt
+
+whoami > me.txt
+
+cp me.txt metoo.txt
+
+man wc > wchelp.txt
+
+cat wchelp.txt
+
+wc -l wchelp.txt | cut -d" " -f1 > wchelp-lines.txt
+
+tac wchelp.txt > wchelp-reversed.txt
+
+cat wchelp.txt wchelp-reversed.txt me.txt metoo.txt wchelp-lines.txt > all.txt
+
+tar -cf result.tar *.txt
+
+gzip result.tar -S .gz
+
+cd ..
+
+if test -f result.tar.gz; then
+    rm result.tar.gz
+fi
+
+mv out/result.tar.gz .
+
+rm -rf out/*
+rmdir out
+
+
